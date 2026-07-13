0882f3ea463187cec3c11a13d8c5cbfef4478604
ArathiRNair <arathiravinair@gmail.com>
2026-07-13 23:16:59 +0530

    add pagr created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import NavigationBar from './components/NavigationBar'
 import Home from './components/Home'
+import AddFlight from './components/AddFlight'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <NavigationBar/>
       <Home/>
+      <AddFlight/>
     </>
   )
 }
